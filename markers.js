108f4cb2f20ca432378bcf53e0d37a7428228d51
BuildTools <unconfigured@null.spigotmc.org>
2019-04-07 01:39:48 -0500

    Test Commit

---
+++ markers.js
@@ -0,0 +1,81 @@
+var markers={
+  "pixelmonmap": [
+    {
+      "groupName": "Citiesbe2e_90e7", 
+      "icon": "icons/marker_city.png", 
+      "createInfoWindow": true, 
+      "displayName": "Cities", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Floroma_Gym2620_90e7", 
+      "icon": "icons/marker_floroma_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Floroma Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Snowbelle_Gym2627_90e7", 
+      "icon": "icons/marker_snowbelle_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Snowbelle Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Spectra_Gyms262e_90e7", 
+      "icon": "icons/marker_spectra_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Spectra Gyms", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Canalave_Gym2635_90e7", 
+      "icon": "icons/marker_canalave_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Canalave Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Avia_Gym263c_90e7", 
+      "icon": "icons/marker_avia_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Avia Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Terrera_Gym2643_90e7", 
+      "icon": "icons/marker_terrera_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Terrera Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Cragspur_Gym264a_90e7", 
+      "icon": "icons/marker_cragspur_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Cragspur Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Chrysalia_Gym2651_90e7", 
+      "icon": "icons/marker_chrysalia_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Chrysalia Gym", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Powerplant2658_90e7", 
+      "icon": "icons/marker_powerplant.png", 
+      "createInfoWindow": true, 
+      "displayName": "Powerplant", 
+      "checked": true
+    }, 
+    {
+      "groupName": "Elite_Four265f_90e7", 
+      "icon": "icons/marker_elite_gym.png", 
+      "createInfoWindow": true, 
+      "displayName": "Elite Four", 
+      "checked": true
+    }
+  ]
+};
---
+++ markersDB.js
@@ -0,0 +1,152 @@
+var markersDB={
+  "Terrera_Gym2643_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -1005, 
+        "hovertext": "Terrera Gym\n", 
+        "z": -174, 
+        "text": "Terrera Gym\n"
+      }
+    ], 
+    "name": "Terrera Gym", 
+    "created": false
+  }, 
+  "Snowbelle_Gym2627_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": 904, 
+        "hovertext": "Snowbelle Gym\n", 
+        "z": 97, 
+        "text": "Snowbelle Gym\n"
+      }
+    ], 
+    "name": "Snowbelle Gym", 
+    "created": false
+  }, 
+  "Floroma_Gym2620_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": 780, 
+        "hovertext": "Floroma Gym\n", 
+        "z": 584, 
+        "text": "Floroma Gym\n"
+      }
+    ], 
+    "name": "Floroma Gym", 
+    "created": false
+  }, 
+  "Powerplant2658_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -229, 
+        "hovertext": "Powerplant\n", 
+        "z": -375, 
+        "text": "Powerplant\n"
+      }
+    ], 
+    "name": "Powerplant", 
+    "created": false
+  }, 
+  "Spectra_Gyms262e_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": 797, 
+        "hovertext": "Spectra Gym\n", 
+        "z": -729, 
+        "text": "Spectra Gym\n"
+      }
+    ], 
+    "name": "Spectra Gyms", 
+    "created": false
+  }, 
+  "Citiesbe2e_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -82, 
+        "hovertext": "Empire City\n", 
+        "z": -25, 
+        "text": "Empire City\n"
+      }, 
+      {
+        "y": 64, 
+        "x": 260, 
+        "hovertext": "Iki Town\n", 
+        "z": -80, 
+        "text": "Iki Town\n"
+      }
+    ], 
+    "name": "Cities", 
+    "created": false
+  }, 
+  "Cragspur_Gym264a_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -795, 
+        "hovertext": "Cragspur Gym\n", 
+        "z": 402, 
+        "text": "Cragspur Gym\n"
+      }
+    ], 
+    "name": "Cragspur Gym", 
+    "created": false
+  }, 
+  "Canalave_Gym2635_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -109, 
+        "hovertext": "Canalave Gym\n", 
+        "z": -869, 
+        "text": "Canalave Gym\n"
+      }
+    ], 
+    "name": "Canalave Gym", 
+    "created": false
+  }, 
+  "Chrysalia_Gym2651_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -70, 
+        "hovertext": "Chrysalia Gym\n", 
+        "z": 943, 
+        "text": "Chrysalia Gym\n"
+      }
+    ], 
+    "name": "Chrysalia Gym", 
+    "created": false
+  }, 
+  "Avia_Gym263c_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -830, 
+        "hovertext": "Avia Gym\n", 
+        "z": -731, 
+        "text": "Avia Gym\n"
+      }
+    ], 
+    "name": "Avia Gym", 
+    "created": false
+  }, 
+  "Elite_Four265f_90e7": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -931, 
+        "hovertext": "Elite Four\n", 
+        "z": 1256, 
+        "text": "Elite Four\n"
+      }
+    ], 
+    "name": "Elite Four", 
+    "created": false
+  }
+};
